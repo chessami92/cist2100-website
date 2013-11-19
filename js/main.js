b81b09caaf36a1dcbd49aea23d69dbe96e6446e5
Josh DeWitt <chessami92@gmail.com>
2013-11-19 13:46:33 -0600

    Added some initial content for all of the pages.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 $( "#header" ).load( "header.html" ).addClass( "header" );
-$( "#sidebar" ).load( "ad.html" ).addClass( "content" );
-$( "#footer" ).html( "Interactive Game Studio, &copy; " + (new Date).getFullYear() ).addClass( "content small centered" );
+$( "#main" ).addClass( "content left" );
+$( "#content" ).addClass( "content" );
+$( "#sidebar" ).load( "ad.html" ).addClass( "content right" );
+$( "#footer" ).html( "Interactive Game Studio, &copy;" + (new Date).getFullYear() ).addClass( "content small centered" );
