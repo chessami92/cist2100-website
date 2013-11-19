a9fe0e9182bf87436e74c2e9d5c52b2074b8ec71
Josh DeWitt <chessami92@gmail.com>
2013-11-19 09:37:37 -0600

    Added a sidebar and headers for each page.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,3 @@
 $( "#header" ).load( "header.html" ).addClass( "header" );
+$( "#sidebar" ).load( "ad.html" ).addClass( "content" );
 $( "#footer" ).html( "Interactive Game Studio, &copy; " + (new Date).getFullYear() ).addClass( "content small centered" );
